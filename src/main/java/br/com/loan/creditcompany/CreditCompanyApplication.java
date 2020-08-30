846d7687fd7a5be64f12ac10303e048d284671d1
Matheus Franco <matheus_2222@hotmail.com>
2020-08-30 18:13:00 -0300

    Create a project

---
+++ src/main/java/br/com/loan/creditcompany/CreditCompanyApplication.java
@@ -0,0 +1,13 @@
+package br.com.loan.creditcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/loan/creditcompany/CreditCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.loan.creditcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
